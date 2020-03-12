f3972d3dc86bb3ae12b7cf84a26a5027f47df162
linyu3294 <lin.yu3@husky.neu.edu>
2020-03-12 12:32:37 -0400

    Updated myScript.sh

--- Assignment1_ShellScript_Assembly/part1/myScript.sh
+++ Assignment1_ShellScript_Assembly/part1/myScript.sh
@@ -5,3 +5,54 @@
 
 # You might start out by first saying the purpose of the script and
 # demonstrating its usage.
+
+echo OFF
+echo START OF THE SCRIPT
+
+echo Welcome to Process Killer
+echo 0 - Internet Explorer
+echo 1 - Chrome
+echo 2 - Firefox
+echo 3 - Lync
+echo 4 - Outlook
+echo 5 - Excel 
+set /p userinput="Please make a selection:"
+
+if /i "%userinput%" == "0" goto zero
+if /i "%userinput%" == "1" goto one
+if /i "%userinput%" == "2" goto two
+if /i "%userinput%" == "3" goto three
+if /i "%userinput%" == "4" goto four
+if /i "%userinput%" == "5" goto five
+echo Invalid Option
+goto end
+
+:zero
+taskkill /f /im "iexplore.exe"
+goto end
+
+:one
+taskkill /f /im "chrome.exe"
+taskkill /f /im "GoogleChromePortable.exe"
+goto end
+
+:two
+taskkill /f /im "firefox.exe"
+taskkill /f /im "FirefoxPortable.exe"
+goto end
+
+:three
+taskkill /f /im "lync.exe"
+goto end
+
+:four
+taskkill /f /im "OUTLOOK.EXE"
+goto end
+
+:five
+taskkill /f /im "EXCEL.exe"
+goto end
+
+
+:end
+echo END OF THE SCRIPT
