b8f72f5e347a845e83e50dcc46fa379099ab8465
linyu3294 <lin.yu3@husky.neu.edu>
2020-03-16 13:39:32 -0400

    Update add.c

--- Lab1_UnixTerminal/part2/add.c
+++ Lab1_UnixTerminal/part2/add.c
@@ -1,1 +1,15 @@
 // Write a C program that calls an add function(long add(long a, long b).
+
+#include <stdio.h>
+
+long add(long a, long b){
+    int result = a + b;
+    return result;
+}
+
+int main() {
+    long numOne = 3;
+    long numTwo = 4;
+    printf("%d\n", add(3, 4));
+    return 0;
+}
