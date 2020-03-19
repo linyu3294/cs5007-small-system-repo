d6834da50c578898455dc85264cf4543562748ec
Yu Lin <lin.yu3@husky.neu.edu>
2020-03-19 19:12:21 -0400

    Completed Assignment 2

--- Assignment2_CompilerTools/parse.c
+++ Assignment2_CompilerTools/parse.c
@@ -1,1 +1,23 @@
 // Implement a working parser in this file that splits text into individual tokens.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+    // character pointer used to iterate through strtok
+    char* iter = NULL;
+
+    // When this following line is called, strtok sets argv[1] as the starting
+    // item.
+    iter = strtok(argv[1], " ");
+   
+    while (iter != NULL) {
+        printf("%s\n", iter);
+        //Using Strtok to assign 'NULL' to end of token
+        iter = strtok(NULL, " ,-");
+    }
+
+    return 0;
+
+}
--- Assignment2_CompilerTools/trace/trace.c
+++ Assignment2_CompilerTools/trace/trace.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+#include <time.h>
+
+// a static global variable used for counting calls.
+static int function_counter = 0;
+
+// using <time.h> objects to initiate time tracking variables.
+clock_t start_t;
+clock_t end_t;
+
+//The syntax __attribute__((constructor)) is used to execute a function when the
+//program starts.
+void __attribute__((constructor)) executeAtStart();
+
+//The syntax __attribute__((destructor)) is used to execute the function when
+//main() function is completed.
+void __attribute__((destructor)) executeAtFinish();
+
 
 __attribute__((no_instrument_function))
 void __cyg_profile_func_enter(void *this_fn, void *call_site){
-
+        function_counter++;
 	printf("Function entered\n");
 
 }
 
 __attribute__((no_instrument_function))
 void __cyg_profile_func_exit(void *this_fn, void *call_site){
-
 	printf("Function exited\n");
 
 }
 
+void executeAtStart() {
+    start_t = clock(); // get current time
+}
+
+void executeAtFinish() {
+    end_t = clock();
+    printf("Function Call Counts = %d\n", function_counter);
+    printf("Total Time =  %f seconds\n" , (double)(start_t - end_t)/(CLOCKS_PER_SEC));
+}
