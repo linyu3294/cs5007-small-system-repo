96f458694cce4b4bb728c6f44f9d19a7378eb367
Yu Lin <lin.yu3@husky.neu.edu>
2020-03-21 19:31:43 -0400

    Completed all examples

--- Lab3_Process_and_Cache/Part1/example3.c
+++ Lab3_Process_and_Cache/Part1/example3.c
@@ -1,1 +1,21 @@
 // Type in and try example3.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid;
+    int x =1;
+
+    pid = fork();
+
+    if (pid=0) {
+        printf("Runing child forever now pid=%d\n",getpid());
+         while(1) {}
+    }else {
+        // Run the parrent
+        printf("Terminating parent pid=%d\n",getpid());
+        exit(0);
+    }
+}
